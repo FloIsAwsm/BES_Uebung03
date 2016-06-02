34ea36d2df9f7c475db73670b795bf7d3a51abca
FloIsAwsm <florian.froestl@gmx.at>
2016-06-02 21:12:44 +0200

    ready for testing

--- sender.c
+++ sender.c
@@ -3,7 +3,33 @@
 
 int main(int argc, char ** argv)
 {
+	// use getopt()
+	const char * appname = *argv;
+	int opt;
 	int size = 0;
+  while ((opt = getopt (argc, argv, argstr)) != -1)
+  {
+    if(opt == 'm')
+    {
+    	char * pEnd = 0;
+		size = strtol(params[i], &pEnd, 10);
+    	if(*pEnd != '\0')
+    	{
+    		print_usage(appname);
+    	}
+    }
+    else
+    {
+    	print_usage(appname);
+    }
+  }
+
+  if(size < 1)
+  {
+  	print_usage(appname);
+  	return EXIT_FAILURE;
+  }
+	
 	return method(size);
 }
 
@@ -16,7 +42,19 @@ int method(int size)
 	
 	do{
 		c = fgetc(stdin);
-	} while(c != 'EOF');
+		memwrite(c);
+	} while(c != EOF);
+
+	detach();
 	
 	return 0;	
+}
+
+void print_usage(const char * appname)
+{
+	if(fprintf(stderr, "USAGE: %s -m <elements>\n", appname) < 0)
+	{
+		fprintf(stderr, "%s: %s\n", appname, stderr(errno));
+		exit(EXIT_FAILURE);
+	}
 }
